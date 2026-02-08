11f1fed8240b0e3115ff11b985e11b9e6c44bd8e
AgsRobert08 <betoazhueyapa@gmail.com>
2026-02-08 12:40:21 -0600

    Uncomment cloudinary and cloudinary_storage settings

--- miepi_ebv/settings.py
+++ miepi_ebv/settings.py
@@ -34,8 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-   # 'cloudinary_storage',
-   # 'cloudinary',
+    'cloudinary_storage',
+    'cloudinary',
 
     'miepi',
 ]
